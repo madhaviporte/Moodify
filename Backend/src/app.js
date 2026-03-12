b891a0704a5d1c75d7eecc5f513d0ce57bac03eb
madhaviporte <madhaviporte2004@gmail.com>
2026-03-12 20:38:27 +0530

    frontend integration complete

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookiePasrser = require("cookie-parser");
 const cors = require("cors")
+const path = require("path")
 
 const app = express()
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }))
+app.use(express.static("./public"))
 
 // Routes
 
